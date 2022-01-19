6237eb4218baf1f53d7b5899f4ca79ae1211cebb
Someguy1834 <TheAwesomer1452@gmail.com>
2022-01-19 12:39:36 -0500

    Fixed formatting and changed config.h

--- config.h
+++ config.h
@@ -1,30 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Ubuntu:size=12",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
-};
+static const char* fonts[] = {
+    "Ubuntu:size=12",
+    "JoyPixels:pixelsize=8:antialias=true:autohint=true"};
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#737373", "#1f1f1f" },
-	[SchemeSel] = { "#000000", "#737373" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+static const char* prompt = NULL; /* -p  option; prompt to the left of input field */
+static const char* colors[SchemeLast][2] = {
+    /*     fg         bg       */
+    [SchemeNorm] = {"#ffffff", "#0f0f0f"},
+    [SchemeSel] = {"#ffffff", "#787878"},
+    [SchemeOut] = {"#ffffff", "#0f0f0f"},
 };
 static const unsigned int alphas[SchemeLast][2] = {
-	/*		fgalpha		bgalphga	*/
-	[SchemeNorm] = { fgalpha, bgalpha },
-	[SchemeSel] = { fgalpha, bgalpha },
-	[SchemeOut] = { fgalpha, bgalpha },
+    /*		fgalpha		bgalphga	*/
+    [SchemeNorm] = {fgalpha, bgalpha},
+    [SchemeSel] = {fgalpha, bgalpha},
+    [SchemeOut] = {fgalpha, bgalpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
